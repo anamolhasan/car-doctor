c31e8e1341278ccd5b5379f517e977ed81d04fc3
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-22 00:43:09 +0600

    rigister added

--- src/app/actions/auth/registerUser.js
+++ src/app/actions/auth/registerUser.js
@@ -1,16 +1,25 @@
 'use server'
-
+import bcrypt from 'bcrypt'
 import dbConnect, { collectionNameObj } from "@/lib/dbConnect"
 
 export const registerUser = async(payload) => {
+    console.log(payload)
     const userCollection = dbConnect(collectionNameObj.userCollection)
 
     // validation
+    const {email, password} = payload
+    if(!email || !password) return null
     const user = await userCollection.findOne({email: payload.email})
 
     if(!user){
+        const hashedPassword = await bcrypt.hash(password, 5)
+        payload.password = hashedPassword
          const result = await userCollection.insertOne(payload)
-         return result
+         // ObjectId কে string এ রূপান্তর করো
+        return {
+            acknowledged: result.acknowledged,
+            insertedId: result.insertedId.toString(),
+        }
     }
 
     return null;
